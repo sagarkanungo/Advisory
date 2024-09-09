954ff1ed55fb9facec69ae641de04d31804181c1
sagarkanungo <sagarkanungo23@gmail.com>
2024-09-09 19:24:48 +0530

    solve error

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { NavItem } from "../interface/NavbarConfig";
 
 function Navbar() {
